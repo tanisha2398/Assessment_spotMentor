ba257cf478091c6e5679ba5a92a4736221125e5b
tanisha2398 <tanishanegipro@gmail.com>
2019-08-26 22:13:31 +0530

    add mainpanel

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -12,7 +12,6 @@ export default class Layout extends React.Component {
   render() {
     return (
       <div className="layout">
-        <h1> Layout </h1>
         <SidePanel />
         <MainPanel />
       </div>
